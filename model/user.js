af9ab8e55179ff951288bd9259b0eaefa24cbae1
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-03-21 16:50:22 -0400

    refactor the code

--- model/user.js
+++ model/user.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-    fullName : {
-        type: String,
-        required: true
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     images: [
-        {
-          path: {
-            type: String,
-          },
-        }
-      ]
-}, { collection: 'userDetails', versionKey: false });
+      {
+        path: {
+          type: String,
+        },
+      },
+    ],
+  },
+  { collection: "userDetails", versionKey: false }
+);
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
